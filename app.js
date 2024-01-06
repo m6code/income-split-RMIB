28ceb1db8229e3332a715819cc9a8d6c00f0e57b
Benjamin SA <benjamynfrank101@gmail.com>
2024-01-06 18:42:30 +0100

    Split Logic

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 console.log("Raise am (*3)");
 
+const incomeField = document.getElementById("income");
+const keepsAmount = document.getElementById("keeps");
+const paybackAmount = document.getElementById("payback");
+
+
+function calculateIncome(income) {
+    if(!isNaN(income)) {
+        const keeps = (1/10) * income;
+        const payback = (2/10) * income;
+        setAmounts(keeps, payback);
+    }
+
+}
+
+function setAmounts(keeps, payback){
+    keepsAmount.textContent = keeps;
+    paybackAmount.textContent = payback;
+}
+
+function updateValues() {
+    income = incomeField.value;
+    calculateIncome(income);
+}
