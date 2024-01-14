1396e68fb3a7900532b150c83c983185abe8e595
Benjamin SA <benjamynfrank101@gmail.com>
2024-01-14 21:34:40 +0100

    calcalculate total

--- app.js
+++ app.js
@@ -3,20 +3,25 @@ console.log("Raise am (*3)");
 const incomeField = document.getElementById("income");
 const keepsAmount = document.getElementById("keeps");
 const paybackAmount = document.getElementById("payback");
-
+const emergencyAmount = document.getElementById("emergency");
 
 function calculateIncome(income) {
     if(!isNaN(income)) {
         const keeps = (1/10) * income;
         const payback = (2/10) * income;
-        setAmounts(keeps, payback);
+        const emergency = (3/10) * income;
+        setAmounts(keeps, payback, emergency);
     }
 
 }
 
-function setAmounts(keeps, payback){
+function setAmounts(keeps, payback, emergency){
     keepsAmount.textContent = keeps;
     paybackAmount.textContent = payback;
+    emergencyAmount.textContent = emergency;
+    const total = keeps + payback + emergency;
+
+    emergencyAmount.append(" " +total); 
 }
 
 function updateValues() {
