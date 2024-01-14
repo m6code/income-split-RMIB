dd346fd485d67a3edcc78cb0670bed488c03df1d
Benjamin SA <benjamynfrank101@gmail.com>
2024-01-14 23:24:21 +0100

    add pwa assets

---
+++ sw.js
@@ -0,0 +1,21 @@
+importScripts( 'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+
+workbox.routing.registerRoute(
+    ({request}) => request.destination === 'image',
+    new workbox.strategies.CacheFirst()
+);
+
+// workbox.routing.registerRoute(
+//     ({request}) => request.destination === "script",
+//     new workbox.strategies.NetworkFirst()
+// );
+
+// workbox.routing.registerRoute(
+//     ({request}) => request.destination === "style",
+//     new workbox.strategies.NetworkFirst()
+// );
+
+// workbox.routing.registerRoute(
+//     ({request}) => request.destination === "document",
+//     new workbox.strategies.NetworkFirst()
+// );
