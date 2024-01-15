3e4925c184276e9ad02278a3b0a5b864f6405df4
Benjamin SA <benjamynfrank101@gmail.com>
2024-01-15 01:00:41 +0100

    initiate caching

--- serviceWork.js
+++ serviceWork.js
@@ -1,5 +1,22 @@
 importScripts( 'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
+
+const CACHE_VERSION = 1;
+const CACHE_NAME = `rmib-cache-v${CACHE_VERSION}`;
+const urlsToCache = [
+    '/',
+    '/app.css',
+    '/app.js',
+    '/index.html'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
     new workbox.strategies.CacheFirst()
@@ -19,3 +36,5 @@ workbox.routing.registerRoute(
     ({request}) => request.destination === "document",
     new workbox.strategies.NetworkFirst()
 );
+
+
