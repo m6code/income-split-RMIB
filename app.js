de8525612439e503b7add0ffec35f1b081086989
Benjamin SA <benjamynfrank101@gmail.com>
2024-04-08 23:43:39 +0100

    Add darkmode system toggle

--- app.js
+++ app.js
@@ -98,3 +98,14 @@ window
     theme.value = isDark ? 'dark' : 'light'
     setPreference()
   })
+
+
+
+const switcher = document.querySelector('#theme-switcher')
+const doc = document.firstElementChild
+
+switcher.addEventListener('input', e =>
+  setTheme(e.target.value))
+
+const setTheme = theme =>
+  doc.setAttribute('color-scheme', theme)
