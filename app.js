feb4e9179036b6785ff376acb5764cac439dbfe2
Benjamin SA <benjamynfrank101@gmail.com>
2024-04-08 22:46:39 +0100

    JS ADDED

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 console.log("Raise am (*3)");
 
 const incomeField = document.getElementById("income");
+
+// Result Card
 const keepsAmount = document.getElementById("keeps");
 const paybackAmount = document.getElementById("payback");
 const emergencyAmount = document.getElementById("emergency");
+const deductiblesAmount = document.getElementById("total");
+const balanceAmount = document.getElementById("balance");
+
+incomeField.addEventListener("keyup", (event) => {
+    if(event.key === "Enter") updateValues();
+})
 
 function calculateIncome(income) {
     if(!isNaN(income)) {
         const keeps = (1/10) * income;
         const payback = (2/10) * income;
         const emergency = (3/10) * income;
-        setAmounts(keeps, payback, emergency);
+        const total = keeps + payback + emergency;
+        const balance = income - total;
+        setAmounts(keeps, payback, emergency, total, balance);
     }
 
 }
 
-function setAmounts(keeps, payback, emergency){
+function setAmounts(keeps, payback, emergency, total, balance){
     keepsAmount.textContent = keeps;
     paybackAmount.textContent = payback;
     emergencyAmount.textContent = emergency;
-    const total = keeps + payback + emergency;
-
-    emergencyAmount.append(" " +total); 
+    deductiblesAmount.textContent = total;
+    balanceAmount.textContent = balance;    
 }
 
 function updateValues() {
