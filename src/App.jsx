f73de76bd5f801f3c292b51325d043a469566356
Benjamin SA <benjamynfrank101@gmail.com>
2024-05-15 15:44:14 +0100

    Add js functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import "./App.css";
 import { Header } from "./components/Header.jsx";
 import { Main } from "./components/Main.jsx";
+import "./App.css";
 
 function App() {
   return (
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,47 @@
+import { useRef, useState } from "react";
+
 export const Main = () => {
+  const [results, setResults] = useState({
+    keeps: 0,
+    payback: 0,
+    emergency: 0,
+    total: 0,
+    balance: 0,
+  });
+
+  const inputValueRef = useRef(null);
+
+  const calculateIncomeSplit = (incme) => {
+    // Regular Expression to replace all commas (,) and whitespaces ( ) characters
+    const regex = /[ ,]+/g;
+    const income = Number(incme.replace(regex, ""));
+    console.log(typeof income);
+    if (!isNaN(income)) {
+      const keeps = (1 / 10) * income;
+      const payback = (2 / 10) * income;
+      const emergency = (3 / 10) * income;
+      const total = keeps + payback + emergency;
+      const balance = income - total;
+      setResults({
+        keeps,
+        payback,
+        emergency,
+        total,
+        balance,
+      });
+    } else alert(`${incme} is not a valid number`);
+  };
+
+  const handleSubmit = () => {
+    calculateIncomeSplit(inputValueRef.current.value);
+  };
+
+  inputValueRef.current.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") handleSubmit();
+  });
+
+  const { keeps, payback, total, emergency, balance } = results;
+
   return (
     <main>
       <div>
@@ -9,33 +52,34 @@ export const Main = () => {
             placeholder="Enter your income for the month"
             type="text"
             maxLength="19"
+            ref={inputValueRef}
           />
         </label>
-        <button onClick="">Split Income Now</button>
+        <button onClick={handleSubmit}>Split Income Now</button>
         <div className="result-card" tabIndex="0">
           <p>
             {"\u20A6"}
-            <span id="keeps">0</span> for keeps
+            <span id="keeps">{keeps}</span> for keeps
           </p>
           <p>
             {"\u20A6"}
-            <span id="payback">0</span> for repayment
+            <span id="payback">{payback}</span> for repayment
           </p>
           <p>
             {"\u20A6"}
-            <span id="emergency">0</span> for emergency
+            <span id="emergency">{emergency}</span> for emergency
           </p>
           <p>
             <strong>
               {"\u20A6"}
-              <span id="total">0</span>
+              <span id="total">{total}</span>
             </strong>{" "}
             is deductibles
           </p>
           <p>
             <strong>
               {"\u20A6"}
-              <span id="balance">0</span>
+              <span id="balance">{balance}</span>
             </strong>{" "}
             is your balance
           </p>
